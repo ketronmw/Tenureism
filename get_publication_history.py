324f893ec118eabdc3ca1e2c1f576bfc4c27b983
Ketron Mitchell-Wynne <ketronmw@gmail.com>
2016-09-15 23:06:55 -0700

    i am a bot

--- get_publication_history.py
+++ get_publication_history.py
@@ -6,16 +6,19 @@ import mysql.connector as mysql
 
 import db_info
 try:
-    import scholar
+    import scholar as scholar
 except:
     raise ImportError('Download scholar.py from '
                       'https://github.com/ckreibich/scholar.py')
+
+
+
 
 class GetPublicationHistory:
 
     """ This uses the scholar.py code, found at the github link above,
     to get the publication history from each of the professor names
-    in the profs_list_unique SQL tablestatistics. It extracts the number
+    in the profs_list_unique SQL table statistics. It extracts the number
     of publications per year, the number of citations per year,
     and the total number of publications. These will be the features
     used to do the modeling. 
@@ -49,8 +52,33 @@ class GetPublicationHistory:
         if self.verbose:
             print 'Using professor names in {} with repeats removed.'.format(self.table)
 
+
+        # Before starting queries, check where we are in the table.
+        con_init = mysql.connect(user=self.user, password=self.password)
+        sql_ = con_init.cursor(buffered=True)
+        sql_.execute('USE profs')
+        # Maybe the table profs_pub doesn't exist yet. 
+        try:
+            sql_.execute("select * from profs_pub order by name desc limit 1;")
+            last_line = sql_.fetchall()
+            last_entry = str(last_line).split(',')[2].replace('u','').strip().replace("'",'') #yuck
+        except:
+            last_entry = None
+        con_init.close()
+        sql_.close()
+
+
         # Loop through each prof.
+        null_list_ct, i = 0, 0
         for (name, campus, dept) in sql:
+
+            # Continue until we get to the last name in the table.
+            if last_entry is not None:
+                if name == last_entry:
+                    last_entry = None
+                i += 1
+                print 'skipping {}'.format(name)
+                continue
 
             querier = scholar.ScholarQuerier()
             settings = scholar.ScholarSettings()
@@ -60,7 +88,9 @@ class GetPublicationHistory:
             query.set_phrase(str(dept))
             querier.send_query(query)
 
+
             if len(querier.articles) > 0:
+                null_list_ct = 0 # Returns nothing after Google figures out I'm a bot.
 
                 items = sorted(list(querier.query.attrs.values()), key=lambda item: item[2])
                 total_pubs = items[0][0]
@@ -70,6 +100,7 @@ class GetPublicationHistory:
                     print 'Skipping: {}, {}, {}'.format(str(name), str(dept), str(campus))
                     continue
 
+                # Collect the years of publications, Number, and Citations.
                 years, citations = [], []
                 for art in querier.articles:
                     try:
@@ -86,13 +117,76 @@ class GetPublicationHistory:
                             if 'list' not in line.strip().split(' ')[1]:
                                 citations.append(line.strip().split(' ')[1])
 
-                    # Remove year duplicates (= number of publications!) and add
-                    # up all citations per year.
+                try:
+                    citations = np.array(citations, dtype=int)
+                except:
+                    citations = np.zeros(len(years))
+                years = np.array(years, dtype=int)
+
+                # Remove year duplicates (= number of publications per year!)
+                # and add up all citations per year.
+                # n_pubs = np.zeros(len(np.unique(years)))
+                # years_unq = n_pubs.copy()
+                # citations_per_year = n_pubs.copy()
+                n_pubs, years_unq, citations_per_year = [],[],[]
+                for iyr, year in enumerate(np.unique(years)):
+                    yr_ind = np.where(years == year)[0]
+                    n_pubs.append(len(yr_ind))
+                    try:
+                        cit_p_y = np.sum(citations[yr_ind])
+                    except:
+                        cit_p_y = 'NULL'
+                    if np.sum(citations) == 0:
+                        cit_p_y = 'NULL'
+
+                    citations_per_year.append(cit_p_y)
+                    years_unq.append(year)
+
+                # Now put this stuff in a table.
+                # Open a new MySQL connection. 
+                con2 = mysql.connect(user=self.user, password=self.password)
+                sql_buff = con2.cursor(buffered=True)
+                sql_buff.execute('USE profs')
+
+                if i == 0:
+                    cmd = ('CREATE TABLE profs_pub (year INT, '
+                           'campus VARCHAR(15), name VARCHAR(90), dept VARCHAR(50),'
+                           'n_pub INT, n_citations INT)')
+                    try:
+                        sql_buff.execute(cmd)
+                        if self.verbose:
+                            print 'Created table profs_pub'
+                    except:
+                        print 'Table "profs_pub" already exitsts, appending...'
+
+                for yr, cit, n_ in zip(years_unq, citations_per_year, n_pubs):
+                    #print yr, str(campus), name, dept, n_, cit
+                    data_cmd = ("""INSERT INTO profs_pub
+                                VALUES ({}, "{}", "{}",
+                                "{}", {}, {});""".format(yr,
+                                                     str(campus),
+                                                     str(name),
+                                                     str(dept),
+                                                     n_,
+                                                     cit))
+                    try:
+                        sql_buff.execute(data_cmd)
+                        con2.commit()
+                    except mysql.Error as err:
+                        con2.rollback()
+                        print 'There was an error with this command (i={}) {}:'.format(i, data_cmd)
+                        print 'Error: {}'.format(err)
 
             else:
                 if self.verbose:
-                    'There are no results for {} with phrase={}'.format(str(name, str(dept)))
+                    'There are no results for {} with phrase={}'.format(str(name), str(dept))
+                null_list_ct += 1
 
-            pdb.set_trace()
+            if null_list_ct > 10:
+                print "Google has probably blocked your IP."
+                print 'Try search with {}'.format(str(name))
+                pdb.set_trace()
 
+            i += 1
+            print i
         return None
