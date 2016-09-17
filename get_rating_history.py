0f652f3946f4169023636056a3071687ce524d7b
Ketron Mitchell-Wynne <ketronmw@gmail.com>
2016-09-16 21:04:55 -0700

    full rating history -- Load More JSON

--- get_rating_history.py
+++ get_rating_history.py
@@ -9,6 +9,7 @@ import json
 import mysql.connector as mysql
 from bs4 import BeautifulSoup
 from lxml import html
+from pprint import pprint 
 
 import db_info
 
@@ -32,6 +33,7 @@ class GetRatingHistory:
         self.password = password
         self.verbose = verbose
         self.pct_found = None
+        self.newtable = 'profs_ratings_dept_full'
 
     def capitalize(self, instring):
 
@@ -88,14 +90,13 @@ class GetRatingHistory:
             for link in br.links():
                 if 'ShowRatings' in str(link):
                     mylinks.append(link)
-                    #br.follow_link(link)
-                    # print link
+
 
             if len(mylinks) < 3:
                 # There is no rating for given prof in this case.
-                if self.verbose:
-                    print "There doesn't appear to be a rmp page for {}".format(name)
-                    print 'Skipping this prof.'
+                #if self.verbose:
+                #    print "There doesn't appear to be a rmp page for {}".format(name)
+                #    print 'Skipping this prof.'
                 overall_rating.append('0.0')
                 dept.append('---')
                 bad_ct += 1
@@ -125,37 +126,11 @@ class GetRatingHistory:
             # If the overall rating on rmp is 0.0 then there are no ratings at all.
             # Otherwise look for the rating as a function of time.
             if overall_rating[i] != '0.0':
-                if self.verbose:
-                    print 'Now look for rating as a funciton of time...'
 
-                # First the current site needs to be fully loaded. UGH.
-                # try_again = True
-                # session = requests.Session() ##
-                # load_more_link = br.links().next().base_url + '#'
-                # response = session.get(load_more_link)
-                # page = BeautifulSoup(response.content)
-                # print page
-                # pdb.set_trace()
-                # while try_again:
-                #     for link in br.links():
-                #         if 'More' in link.text:
-                #             mylinks.append(link)
-                #             try_again = True
-                #             continue
-
-                #             lct += 1
-                #             raw = br.response().read()
-                #             soup = BeautifulSoup(raw)
-                #             tree = html.fromstring(raw)
-                #             #print soup
-                #             #print '===================================================='
-                #             comments=tree.xpath('//p[@class="commentsParagraph"]/text()')
-                #             print len(comments)
-                #             pdb.set_trace()
-                #             br.follow_link(link)
-                #             print 'Loading more ({})'.format(lct)
-                #         else:
-                #             try_again = False
+                # HTML parsing.
+                # Here we scrape the first page before the "load more" button is clicked.
+                # In order to click that button we have to execute some javascript, of
+                # which the output is different. 
                 linknum = 1
                 raw = br.response().read()
                 soup = BeautifulSoup(raw)
@@ -163,14 +138,40 @@ class GetRatingHistory:
                 #comments = tree.xpath('//p[@class="commentsParagraph"]/text()')
                 #difficulty = tree.xpath('//span[@class="score inverse good"]/text()')
 
-                # Scores have different labels ('average', 'poor' etc.), so need
-                # to get rating with associated date with find_next().
+                # Scores have different labels ('average', 'poor' etc.), so I need
+                # to get rating with associated date, with find_next().
                 years_tot, ratings_tot = [], []
                 dates = soup.find_all('div',class_='date')
                 for date in dates:
                     ratings_tot.append(float(date.find_next(class_='score').text))
                     years_tot.append(str(date.text.strip()[-4::]))
- 
+                # Now I have the ratings as a function of time for the first page.
+
+
+                ######### JSON ############ "LOAD MORE" button
+                ## Now execute javascript "load more" button to get all the results, not
+                # just the first 20! Load up to page 40 - that's a lot - it'll fail before then.
+                base_url = br.find_link().base_url
+                tid = base_url[base_url.find('tid')+4:] #get prof ID from url. it's 4 char after 'tid'.
+
+                for pgnum in range(2,41):
+                    params = {'page':pgnum, 'tid':tid}
+                    json_cmd = 'https://www.ratemyprofessors.com/paginate/professors/ratings'
+                    loadmore = requests.get(json_cmd, params=params)
+                    json_data = loadmore.json() #This is awesome!!!
+
+                    jdat = json_data['ratings'] #Give list! Should be len(20)
+                    if len(jdat) == 0: # Nothing more to load.
+                        #if self.verbose:
+                            #print 'Entries for prof {} end at page {}. Check.'.format(str(name), pgnum)
+                        break
+
+                    for entry in jdat:
+                        # Entry is a dict! Just append to variables with first 20 entries.
+                        years_tot.append(str(entry['rDate'])[-4:])
+                        ratings_tot.append(float(entry['rOverall']))
+
+
                 # For each year just take the average score. Although this
                 # is throwing away data so it could be changed.
                 years, ratings = [], []
@@ -182,10 +183,10 @@ class GetRatingHistory:
 
                 # Now we have ratings as a function of year. These, along with
                 # department + prof name should be saved to a new SQL table!
+                # But first! - the load more button needs to be clicked.
                 if self.verbose and i == 0:
                     print ("Generating new SQL table with ratings + department,"
-                           "called profs_ratings_dept")
-
+                           "called {}".format(self.newtable))
                 # New table command.
                 if i == 0:
                     # Open a new MySQL connection. 
@@ -193,25 +194,27 @@ class GetRatingHistory:
                     sql_buff = con2.cursor(buffered=True)
                     sql_buff.execute('USE profs')
 
-                    cmd = ('CREATE TABLE profs_ratings_dept (year INT, '
+                    cmd = ('CREATE TABLE {} (year INT, '
                            'campus VARCHAR(15), name VARCHAR(90), avg_rating FLOAT,'
-                           'dept VARCHAR(50))')
+                           'dept VARCHAR(50))'.format(self.newtable))
                     try:
                         sql_buff.execute(cmd)
                     except:
-                        print 'Table "{}"already exitsts, appending...'.format(self.table)
+                        print 'Table "{}"already exitsts, appending...'.format(self.newtable)
                         #print 'Dropping ratings DB.'
-                        #sql_buff.execute('DROP TABLE profs_ratings_dept;')
+                        #sql_buff.execute('DROP TABLE {};'.format(self.newtable))
                         #sql_buff.execute(cmd)
 
                 # Insert data for each year into table. Each year = 1 row.
                 for year, rating in zip(years, ratings):
-                    data_cmd = ("""INSERT INTO profs_ratings_dept
-                                 VALUES ({}, "{}", "{}", {}, "{}");""".format(year,
-                                                              str(campus).lower().replace(' ','_'),
-                                                              str(name),
-                                                              rating,
-                                                              the_dept))
+                    data_cmd = ("""INSERT INTO {}
+                                 VALUES ({}, "{}", "{}",
+                                 {}, "{}");""".format(self.newtable,
+                                                      year, 
+                                                      str(campus).lower().replace(' ','_'),
+                                                      str(name),
+                                                      rating,
+                                                      the_dept))
                     try:
                         sql_buff.execute(data_cmd)
                         con2.commit()
@@ -219,6 +222,9 @@ class GetRatingHistory:
                         con2.rollback()
                         print 'There was an error with this command (i={}) {}:'.format(i, data_cmd)
                         print 'Error: {}'.format(err)
+
+
+
 
             i+=1
             print i
